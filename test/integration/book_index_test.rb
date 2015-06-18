d4f91845d3ff1d73d7c533e7e52808d54d46d283
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-18 14:13:36 -0700

    testing book get request

--- test/integration/book_index_test.rb
+++ test/integration/book_index_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class BookIndexTest < ActionDispatch::IntegrationTest
+  test 'listing all the books' do
+    get "/api/v1/books"
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    books = json(response.body)
+    assert_equal 2, books.size
+  end
 end
