c8e1cc2bbbb91f23f6ea2c8b6bf43eb52ea866af
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-18 14:14:01 -0700

    testing book post request

--- test/integration/create_book_test.rb
+++ test/integration/create_book_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class CreatingBookTest < ActionDispatch::IntegrationTest
-  test 'checking config' do
-    assert_equal 4, 2 + 2
-  end
-
-  test 'second check' do
-    assert_not_equal 10, 2 + 2
+  test 'creating a new book' do
+    post "/api/v1/books",
+      { title: 'The Stranger', author: "Albert Camus", isbn: 8438384,
+        review: "Existentialism at its finest", rating: 5,
+        genre: "{'fiction' => true}" }
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+    book = json(response.body)
+    assert_equal "The Stranger", book[:title]
+    assert_equal 5, book[:rating]
   end
 end
