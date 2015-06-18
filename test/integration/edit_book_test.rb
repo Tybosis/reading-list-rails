435a27877af9e929656e23122ac6c4c3a9d5a446
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-18 14:14:39 -0700

    testing book patch request

--- test/integration/edit_book_test.rb
+++ test/integration/edit_book_test.rb
@@ -1,4 +1,21 @@
 require 'test_helper'
 
 class EditBookTest < ActionDispatch::IntegrationTest
+  test 'editing an existing book' do
+    book = Book.first
+    patch "/api/v1/books/#{book.id}", { title: "The Pragmatic Programmer",
+                                            author: "Andy Hunt and Dave Thomas" },
+           { 'Accept' => 'application/json',
+             'Content_type' => 'application/json' }
+    assert_equal 200, response.status
+    assert_equal 'The Pragmatic Programmer', book.reload.title
+    assert_equal 'Andy Hunt and Dave Thomas', book.reload.author
+  end
+
+  test 'editing non existant book returns 404' do
+    patch "/api/v1/books/57", { title: "HTML for Babies" },
+          { 'Accept' => 'application/json',
+            'Content_type' => 'application/json' }
+    assert_equal 404, response.status
+  end
 end
