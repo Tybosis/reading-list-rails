f3bc07ee54f9f47d567f8b7623f4207e46a26dbd
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-18 00:22:47 -0700

    blank test files to be filled out

---
+++ test/integration/book_index_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BookIndexTest < ActionDispatch::IntegrationTest
+end
---
+++ test/integration/edit_book_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class EditBookTest < ActionDispatch::IntegrationTest
+end
