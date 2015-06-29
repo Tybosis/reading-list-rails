ed3477fc2a1f17bc13b7ac4cedcb8033a0c92601
Tyler Pottle <tyler.pottle@gmail.com>
2015-06-29 16:21:17 -0700

    changed some quotes

--- test/integration/edit_book_test.rb
+++ test/integration/edit_book_test.rb
@@ -5,8 +5,8 @@ class EditBookTest < ActionDispatch::IntegrationTest
     book = Book.first
     patch "/api/v1/books/#{book.id}", { title: "The Pragmatic Programmer",
                                             author: "Andy Hunt and Dave Thomas" },
-           { 'Accept' => 'application/json',
-             'Content_type' => 'application/json' }
+           { "Accept" => "application/json",
+             "Content_type" => "application/json" }
     assert_equal 200, response.status
     assert_equal 'The Pragmatic Programmer', book.reload.title
     assert_equal 'Andy Hunt and Dave Thomas', book.reload.author
@@ -14,8 +14,8 @@ class EditBookTest < ActionDispatch::IntegrationTest
 
   test 'editing non existant book returns 404' do
     patch "/api/v1/books/57", { title: "HTML for Babies" },
-          { 'Accept' => 'application/json',
-            'Content_type' => 'application/json' }
+          { "Accept" => "application/json",
+            "Content_type" => "application/json" }
     assert_equal 404, response.status
   end
 end
