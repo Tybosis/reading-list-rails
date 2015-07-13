aa1ad96e7eae747ef1112e5c075ad4c1a3a275d8
Tyler Pottle <tyler.pottle@gmail.com>
2015-07-12 18:31:03 -0700

    add devise methods to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,10 @@ module API
   module V1
     class UsersController < API::V1::BaseController
       acts_as_token_authenticatable
+
+      devise :invitable, :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable
     end
   end
 end
